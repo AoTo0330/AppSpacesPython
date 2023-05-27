0ac3865d9819461f4060c9bac6e79268499ba952
aotolog <68062035+AoTo0330@users.noreply.github.com>
2023-05-27 17:12:59 +0900

    Update App.tsx to add datadogRum

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -10,6 +10,26 @@ import { initializeIcons } from '@fluentui/react/lib/Icons';
 import { ThemeProvider } from '@fluentui/react';
 import Telemetry from './components/telemetry';
 
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: '223fa8ce-0bf7-4c05-b305-70c0d3d7e736',
+    clientToken: 'pubeb3b240651ac1d9a19858e865d7a23f4',
+    site: 'datadoghq.com',
+    service:'todolist',
+    env:'none',
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    // version: '1.0.0',
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel:'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
+
 export const App: FC = () => {
   const defaultState: ApplicationState = getDefaultState();
   const [applicationState, dispatch] = useReducer(appReducer, defaultState);
